14dfb9df20bad42fee9e0e46b83f4113a9f5f0e8
Ghaith <Ghaith.hammadi@esprit.tn>
2018-02-02 16:46:07 +0100

    first

---
+++ Assets/Assets/Scripts/changeSceneWithNum.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeSceneWithNum : MonoBehaviour {
+
+	public string numPerson;
+	public string scene1 = "Assets/Scenes/1";
+	public string scene2 = "Assets/Scenes/2";
+	public string scene3 = "Assets/Scenes/3";
+	public string scene4 = "Assets/Scenes/4";
+
+
+	public RadialMenu radialMenu;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	//	GameObject mainMenuObject = GameObject.FindWithTag ("radialMenuButton");
+//		radialMenu = mainMenuObject.GetComponent<RadialMenu> ();
+		//GameObject mainMenu = GameObject.FindGameObjectsWithTag("MainMenu");
+		//RadialMenu script = mainMenu.GetComponent<RadialMenu>();
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		numPerson = radialMenu.whatNumIsSelected;
+		//print ("--------"+numPerson);
+		//print ("++++++++"+radialMenu.whatNumIsSelected);
+	}
+
+	public void Try_it_change_Scene_AA(string x){
+		if(x.Equals("1"))
+			SceneManager.LoadScene(scene1);
+		if(x.Equals("2"))
+			SceneManager.LoadScene(scene2);
+		if(x.Equals("3"))
+			SceneManager.LoadScene(scene3);
+		if(x.Equals("4"))
+			SceneManager.LoadScene(scene4);
+	}
+}
---
+++ Assets/Assets/Scripts/testJson.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NiceJson;
+
+
+public class testJson : MonoBehaviour {
+
+
+
+
+
+
+
+	void Start () 
+	{
+
+
+		string url = "http://192.168.1.2:8080/api/getrestaurantcategories/1";
+		WWW www = new WWW(url);
+		StartCoroutine(WaitForRequest(www));
+	}
+	IEnumerator WaitForRequest(WWW www)
+	{
+		yield return www;
+
+		// check for errors
+		if (www.error == null){
+			Debug.Log("WWW Ok!: " + www.data);
+		} else {
+			Debug.Log("WWW Error: "+ www.error);
+		}
+
+		JsonArray arrayExample = (JsonArray) JsonNode.ParseJsonString(www.data);
+		Debug.Log (arrayExample [0] ["name"]+"--------"+ arrayExample.Count);
+
+		//meal = JsonUtility.FromJson<Meal>(www.data);
+		//meal = (Meal)JsonUtility.FromJson(www.data, typeof(Meal));
+		//Debug.Log("---------------"+meal.id_restaurant);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
